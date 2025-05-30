43cb022f2fa5894688360fc67b5e1f63825cce05
Tony Tran <thanhtan.tran@gmail.com>
2025-05-31 01:15:58 +0700

    add C demo

---
+++ demo/build.sh
@@ -0,0 +1,10 @@
+set -e
+rm -rf build
+mkdir build && cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_SYSTEM_NAME=Linux \
+        -DCMAKE_SYSTEM_PROCESSOR=aarch64 \
+
+make -j8
+make install
